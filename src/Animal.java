1a2cd35225ce8b892e4503aa762cf3e38fb1860f
Auberon Lopez <auberon.lopez@greenriver.edu>
2024-10-03 08:32:37 -0700

    Added Javadoc to Animal interface

--- src/Animal.java
+++ src/Animal.java
@@ -1,3 +1,23 @@
+/**
+ * The Animal interface represents any animal that can be displayed as ASCII art.
+ * Classes implementing this interface should provide their specific animal artwork
+ * and override the toString method to return the name of the animal.
+ */
 interface Animal {
+
+  /**
+   * Returns the ASCII art representation of the animal.
+   *
+   * @return A string containing the ASCII art of the animal.
+   */
   String getAnimalArt();
+
+  /**
+   * Implementing classes should override the toString method to return the
+   * name of the animal.
+   *
+   * @return The name of the animal.
+   */
+  @Override
+  String toString();
 }
