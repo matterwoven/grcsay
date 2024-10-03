20225168b82da38c9812f13df2c4857ec590e0cb
Auberon Lopez <auberon.lopez@greenriver.edu>
2024-10-03 08:34:15 -0700

    Javadoc'd Cow Class

--- src/Cow.java
+++ src/Cow.java
@@ -1,4 +1,16 @@
+/**
+ * The Cow class represents a cow and provides its ASCII art representation.
+ * This class implements the Animal interface and overrides the getAnimalArt 
+ * and toString methods.
+ */
 class Cow implements Animal {
+
+  /**
+   * Returns the ASCII art representation of the cow.
+   *
+   * @return A string containing the ASCII art of the cow.
+   */
+  @Override
   public String getAnimalArt() {
       return "        \\   ^__^\n" +
              "         \\  (oo)\\_______\n" +
@@ -7,6 +19,13 @@ class Cow implements Animal {
              "                ||     ||";
   }
 
+  /**
+   * Returns the name of the animal ("cow").
+   * This method overrides the toString method to return the name of the cow.
+   *
+   * @return The string "cow" representing the name of the animal.
+   */
+  @Override
   public String toString() {
     return "cow";
   }
