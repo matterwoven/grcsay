7012cbf42bcfa3748f394149da1b1b3639ba147d
Matthew Vargas <145516753+matterwoven@users.noreply.github.com>
2025-10-02 14:57:20 -0700

    Changes made to sayapp regarding deer

--- src/SayApp.java
+++ src/SayApp.java
@@ -76,7 +76,7 @@ public class SayApp {
      * @return A list of Animal objects.
      */
     public static List<Animal> animalList() {
-      return Arrays.asList(new Cow(), new Duck());
+      return Arrays.asList(new Cow(), new Duck(), new Deer());
     }
 
     /**
